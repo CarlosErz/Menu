0f9111dfa287198641721010db165def7a8c1fbc
CarlosErz <rosariosalda25@gmail.com>
2023-08-08 16:43:36 -0500

    btn and modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,20 +5,27 @@ export function Card({ TitleCard, DescriptionCard, PriceCard, ImgCard,onClick })
 
   return (
     <>
-      <section className="Card" onClick={onClick}>
+      <section className="Card" >
 
 
-          <header className="Card_Content">
+          <header className="Card_Content" onClick={onClick}>
             <div className="Card_img_content">
                <img src={ImgCard} alt={TitleCard} className="Card_img" />
             </div>
           
           <h1 className="Card_title">{TitleCard}</h1>
           <p className="Card_description">{DescriptionCard}</p>
-          <p className="Card_price">
+         
+          
+          </header>
+           <div className="Card_flex">
+            <button className="Card_agg">
+              +
+            </button>
+            <p className="Card_price">
             {PriceCard}
           </p>
-          </header>
+          </div>
       </section> 
     </>
   )
