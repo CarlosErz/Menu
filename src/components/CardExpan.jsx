ed962c5d1077fab26156b1dfa0b89f077874ea3a
CarlosErz <rosariosalda25@gmail.com>
2023-08-08 09:36:11 -0500

    price in spanish

--- src/components/CardExpan.jsx
+++ src/components/CardExpan.jsx
@@ -14,7 +14,7 @@ export function CardExpan({ selectedCard , onclick}) {
       <button className="Card_expan_btn"  onClick={onclick} >X</button>
       <h2 className='Card_expan_title'>{selectedCard.TitleCard}</h2>
       <p className='Card_expan_p'>{selectedCard.DescriptionCard}</p>
-      <p className='Card_expan_p'>Price: <b>{selectedCard.PriceCard}</b> </p>
+      <p className='Card_expan_p'>Precio: <b>{selectedCard.PriceCard}</b> </p>
       <img className='Card_expan_img' src={selectedCard.ImgCard} alt={selectedCard.TitleCard} />
     </section>
     </div>
